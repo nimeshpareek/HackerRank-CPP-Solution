ee706a3d23510f9645c3ade861ff3fb9ed2ece58
nimeshpareek <72460673+nimeshpareek@users.noreply.github.com>
2021-07-06 09:42:01 +0530

    Add files via upload

---
+++ input output.cpp
@@ -0,0 +1,15 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int x,y,z,sum;
+    cin >> x >> y >> z;
+    sum = x + y + z;
+    cout << sum;
+    return 0;
+}
