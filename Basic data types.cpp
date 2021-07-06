379ad92b2eb8e18b43b4620e7e474a78fa582bc3
nimeshpareek <72460673+nimeshpareek@users.noreply.github.com>
2021-07-06 22:48:46 +0530

    Add files via upload

---
+++ Basic data types.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdio>
+#include<math.h>
+#include <iomanip> 
+using namespace std;
+
+int main() {
+    int x;
+    long long y;
+    char z;
+    float q;
+     double w;
+
+  cin >> x >> y >> z >> q >> w;
+   cout << x << endl;
+    cout << y << endl;
+  cout << z << endl;
+  cout << fixed;
+    cout << setprecision(3) << q<< endl;
+  cout << setprecision(9) << w<< endl;
+  
+  return 0;
+}
